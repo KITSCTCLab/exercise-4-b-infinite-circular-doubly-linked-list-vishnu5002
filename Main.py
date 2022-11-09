fdc5cdef81acb7327e563e39d46126219dfb74db
vishnu5002 <110230643+vishnu5002@users.noreply.github.com>
2022-11-09 09:45:42 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,19 @@
-# Read an integer that denotes the length of the list which is returned as the output of the algorithm
+""" circular doubly linked list """
+
+
 length_of_circular_linked_list = int(input())
-# Read space-separated integers that denote the elements of the list which is returned as the output of the algorithm
+
 circular_linked_list = list(map(int,input().strip().split(" ")))
-# Write your code here
+
+
+actual_list = []
+
+index = 0
+while len(actual_list) < length_of_circular_linked_list and index < len(circular_linked_list):
+    element = circular_linked_list[index]
+    if element not in actual_list:
+        actual_list.append(element)
+    index += 1
+
+print(len(actual_list))
+print(" ".join(str(num) for num in actual_list))
